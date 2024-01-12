da450fc74d3c218c5ed56b6c96d8ea3f2cb8a57e
Timur <raxmatovtimurjava@gmail.com>
2024-01-12 16:40:08 +0500

    added signUp design

---
+++ src/Components/Forms/SignUpForm/index.tsx
@@ -0,0 +1,136 @@
+import { AccountCircle } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const SignUpForm = () => {
+  return (
+    <Box sx={{marginTop: '0.3rem'}}>
+      <FormControl
+        sx={{ width: "100%", padding: "0.5rem 0" }}
+      >
+        <FormLabel
+          sx={{
+            marginTop: "0.7rem",
+            marginBottom: "-0.5rem",
+            color: "#000",
+          }}
+          htmlFor="email"
+        >
+          E-mail or phone number
+        </FormLabel>
+        <TextField
+          variant="outlined"
+          sx={{ padding: "0.7rem 0" }}
+          required
+          fullWidth
+          id="email"
+          name="email"
+          autoComplete="phoneNumber"
+          autoFocus
+          placeholder="Email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <FormLabel
+          sx={{
+            marginTop: "0.7rem",
+            marginBottom: "-0.5rem",
+            color: "#000",
+          }}
+          htmlFor="password"
+        >
+          Password
+        </FormLabel>
+        <TextField
+          variant="outlined"
+          sx={{ padding: "0.7rem 0" }}
+          required
+          fullWidth
+          id="password"
+          name="password"
+          autoComplete="phoneNumber"
+          autoFocus
+          placeholder="Password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <FormLabel
+          sx={{
+            marginTop: "0.7rem",
+            marginBottom: "-0.5rem",
+            color: "#000",
+          }}
+          htmlFor="reapetPassword"
+        >
+          Repeat Password
+        </FormLabel>
+        <TextField
+          variant="outlined"
+          sx={{ padding: "0.7rem 0" }}
+          required
+          fullWidth
+          id="reapetPassword"
+          name="reapetPassword"
+          autoComplete="phoneNumber"
+          autoFocus
+          placeholder="Repeat Password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          sx={{
+            padding: "0.7rem 0",
+            textAlign: "center",
+            width: "100%",
+            backgroundColor: "#2F93F6",
+            color: "#fff",
+            textTransform: "none",
+            marginTop: "1rem",
+          }}
+        >
+          Sign Up
+        </Button>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#009EF7",
+            textAlign: "end",
+            padding: "0.3rem 0",
+            "&:hover": {
+              color: "#009cf75c",
+              transition: "0.4s",
+            },
+          }}
+        >
+          Forgot Password?
+        </Typography>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default SignUpForm;
--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,5 +1,67 @@
+import { Box, Container, Typography } from "@mui/material";
+import { AuthorizationButton } from "../../Components/Buttons/Authorization";
+import { FacebookRounded, Google } from "@mui/icons-material";
+import SignUpForm from "../../Components/Forms/SignUpForm";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Container>
+        <Box sx={{ width: "100%" , display: 'flex' , alignItems: 'center' , justifyContent: 'center' , height: '100vh' , }}>
+          <Box
+            sx={{
+              width: "500px",
+              padding: "1.5rem",
+              backgroundColor: "#fff",
+              boxShadow: "0px 0px 20px #9c989838",
+              borderRadius: '13px'
+            }}
+          >
+            <Typography sx={{ textAlign: "center" , fontWeight: "600" }} variant="h6">
+              Sign up for an account
+            </Typography>
+            <Typography
+              sx={{ textAlign: "center", color: "#A1A5B7" , padding: "0.3rem 0" }}
+              variant="body1"
+            >
+              Your social campaigns
+            </Typography> 
+            <Box sx={{ display: "flex", justifyContent: "space-between" , padding: '0.5rem 0' , gap: '0.5rem' }}>
+              <AuthorizationButton>
+                <Google /> Sign up with Google
+              </AuthorizationButton>
+              <AuthorizationButton>
+                <FacebookRounded /> Sign up with Apple
+              </AuthorizationButton>
+            </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: "0.4rem",
+              marginTop: '0.4rem'
+              
+            }}
+          >
+            <Box
+              sx={{ width: "37%", height: "1px", backgroundColor: "#EFF2F5" }}
+            ></Box>
+            <Typography sx={{fontSize: '15px' , color: "#A1A5B7"}} variant="body2">Or with email</Typography>
+            <Box
+              sx={{ width: "37%", height: "1px", backgroundColor: "#EFF2F5" }}
+            ></Box>
+          </Box>
+          <SignUpForm />
+          <Typography variant="body1" sx={{display: "flex" , alignItems: 'center' , justifyContent: 'center' , gap: '0.5rem'}}>
+          Already have an Account? <Link className="SignInlink" to='/signin'>Sign In</Link>
+          </Typography>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
 export default SignUp;
