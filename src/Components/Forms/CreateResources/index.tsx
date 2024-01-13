4e3411fe477ee79b409b73b50cae5d091c84fbe8
Timur <raxmatovtimurjava@gmail.com>
2024-01-13 18:59:35 +0500

    edit resources create page

--- src/Components/Forms/CreateResources/index.tsx
+++ src/Components/Forms/CreateResources/index.tsx
@@ -20,37 +20,55 @@ const CreateResourcesForm = () => {
         }}
       >
         <Box sx={{ display: "flex", width: "100%", gap: "1rem" }}>
-          <Box sx={{ width: "50%" , display: "flex" , flexDirection: "column" }}>
+          <Box sx={{ width: "50%", display: "flex", flexDirection: "column" }}>
             <FormLabel>Fan</FormLabel>
-            <Select style={{height: "40px" , width: "100%"}}  >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+            <Select
+              style={{ height: "40px", width: "100%" }}
+              placeholder="Fani tanlang"
+            >
+              <MenuItem value={10}>Ingliz tili</MenuItem>
+              <MenuItem value={20}>Algebra</MenuItem>
+              <MenuItem value={30}>Geometriya</MenuItem>
+              <MenuItem value={40}>Fizika</MenuItem>
+              <MenuItem value={50}>Kimyo</MenuItem>
+              <MenuItem value={60}>Tarix</MenuItem>
+              <MenuItem value={70}>Rus tili</MenuItem>
             </Select>
-
-            
           </Box>
           <Box sx={{ width: "50%" }}>
             <FormLabel id="sinf">Sinf</FormLabel>
-            <Select style={{height: "40px" , width: "100%"}} name="sinf" id="sinf" >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+            <Select
+              style={{ height: "40px", width: "100%" }}
+              placeholder="Sinfni kiriting"
+              name="sinf"
+              id="sinf"
+            >
+              <MenuItem value={10}>1-sinf</MenuItem>
+              <MenuItem value={20}>2-sinf</MenuItem>
+              <MenuItem value={30}>3-sinf</MenuItem>
+              <MenuItem value={40}>4-sinf</MenuItem>
+              <MenuItem value={50}>5-sinf</MenuItem>
+              <MenuItem value={60}>6-sinf</MenuItem>
             </Select>
           </Box>
         </Box>
         <Box sx={{ display: "flex", width: "100%", gap: "1rem" }}>
           <Box sx={{ width: "50%" }}>
             <FormLabel>Mavzu</FormLabel>
-            <Select style={{height: "40px" , width: "100%"}}  >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+            <Select
+              style={{ height: "40px", width: "100%" }}
+              placeholder="Mavzuni nomini yozing"
+              labelId="monday_select"
+            >
+              <MenuItem value={10}>Ratsional Tengsizliklar</MenuItem>
+              <MenuItem value={20}>Tenglamalar</MenuItem>
+              <MenuItem value={30}>Kvadrat tenglamalar</MenuItem>
             </Select>
           </Box>
           <Box sx={{ width: "50%" }}>
             <FormLabel>Resurs nomi</FormLabel>
             <TextField
+              type="file"
               InputProps={{ style: { height: "40px" } }}
               sx={{ width: "100%" }}
               placeholder="Resurs nomini kiriting"
