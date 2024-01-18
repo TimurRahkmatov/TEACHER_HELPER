89f932232230b64b0bbf75e53eff61d2f7f0c2b5
Timur <raxmatovtimurjava@gmail.com>
2024-01-18 19:15:07 +0500

    edit moderator by id model

--- src/Components/Tables/ModeratorTable/index.tsx
+++ src/Components/Tables/ModeratorTable/index.tsx
@@ -5,112 +5,158 @@ import {
   Tv,
   Visibility,
 } from "@mui/icons-material";
-import {
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const ModeratorTable = () => {
-  function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  ];
-
   return (
-    <Box sx={{ marginTop: "2rem" }}>
-      <TableContainer>
-        <Table
+    <Box
+      sx={{
+        marginTop: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "0.4rem 2rem",
+          backgroundColor: "#fff",
+          borderRadius: "7px",
+          flexDirection: {
+            md: "initial",
+            sm: "column ",
+            xs: "column",
+          },
+          height: { lg: "170px", md: "170px", sm: "400px", xs: "400px" },
+          width: { lg: "100%", md: "100%", sm: "80%", xs: "80%" },
+        }}
+      >
+        <Box
           sx={{
-            minWidth: 650,
-            border: "none",
-            backgroundColor: "#fff",
-            borderRadius: "10px",
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            gap: "1rem",
+            alignItems: "center",
+            height: "100%",
           }}
-          aria-label="simple table"
         >
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">#</TableCell>
-              <TableCell align="center">Mavzu nomi</TableCell>
-              <TableCell align="center">Resurslar turlari</TableCell>
-              <TableCell align="center">Resurslar soni</TableCell>
-              <TableCell align="center">Harakatlar</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          <Box sx={{ textAlign: "start" }}>
+            <Typography>Mavzu nomi</Typography>
+          </Box>
+          <Box sx={{ margin: "auto" }}>
+            <Typography
+              sx={{ fontSize: "17px", fontWeight: "600", maxWidth: "520px" }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit id
+              quidem distinctio pe
+            </Typography>
+            <Box sx={{ display: "flex", gap: "0.8rem", color: "grey" }}>
+              <Typography>Algebra </Typography>
+              <Typography>1*sinf</Typography>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: { lg: "space-between", md: "space-between" },
+            flexDirection: { lg: "column", md: "column" },
+            gap: "0.5rem",
+            alignItems: "center",
+            height: "100%",
+            width: { lg: "200px", md: "200px", sm: "100%", xs: "100%" },
+          }}
+        >
+          <Box sx={{ textAlign: "start" }}>
+            <Typography>Resurs turi</Typography>
+          </Box>
+          <Box
+            sx={{
+              margin: { lg: "auto", md: "auto", sm: "0", xs: "0" },
+              display: { lg: "block", md: "block", sm: "flex", xs: "flex" },
+              justifyContent: "start",
+            }}
+          >
+            <Box>
+              {" "}
+              <Description /> <Airplay />
+            </Box>
+            <Box>
+              {" "}
+              <Tv /> <Cloud />
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            // justifyContent: "space-between",
+            flexDirection: {
+              lg: "column",
+              md: "column",
+            },
+            gap: "0.5rem",
+            alignItems: "center",
+            width: { lg: "200px", md: "200px", sm: "100%", xs: "100%" },
+            height: "100%",
+          }}
+        >
+          <Box>
+            <Typography sx={{ textAlign: "start" }}>Resurs turi</Typography>
+          </Box>
+          <Box sx={{ margin: { lg: "auto", md: "auto", sm: "0", xs: "0" } }}>
+            <Typography sx={{ textAlign: "center", margin: "auto" }}>
+              2ta
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: {
+              lg: "space-between",
+              md: "space-between",
+              sm: "start",
+              xs: "start",
+            },
+            flexDirection: { lg: "column", md: "column" },
+            height: "100%",
+            gap: { lg: "0", md: "0", sm: "1rem", xs: "1rem" },
+            width: { lg: "100px", md: "100px", sm: "100%", xs: "100%" },
+          }}
+        >
+          <Box sx={{ textAlign: "start" }}>
+            <Typography>Harakatlar</Typography>
+          </Box>
+          <Box sx={{ margin: { lg: "auto", md: "auto", sm: "0", xs: "0" } }}>
+            <Link to='/recources/science/class/moderator/science/1'>
+              <Button
+                sx={{
+                  backgroundColor: "#40BFC1",
+                  color: "#fff",
+                  padding: "0.5rem 1rem",
+                  borderRadius: "20px",
+                  marginBottom: "5px",
+                  "&:hover": {
+                    color: "#40BFC1",
+                    border: "1px solid #40BFC1",
+                    backgroundColor: "#fff",
+                    transition: "0.4s",
+                  },
+                }}
               >
-                <TableCell align="center">#1</TableCell>
-                <TableCell align="inherit" component="th" scope="row">
-                  <Box>
-                    <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
-                      {" "}
-                      <Link style={{color: "inherit" , textDecoration: "none"}} to='/recources/science/class/moderator/science/1'>
-                        Старший учитель математики учитель математики Старший
-                        учитель математикиds
-                      </Link>
-                    </Typography>
-                  </Box>
-                  <Typography sx={{ textAlign: "start" }}>
-                    Algebra 1*sinf
-                  </Typography>
-                </TableCell>
-                <TableCell align="center">
-                  <Box>
-                    <Description /> <Airplay />{" "}
-                  </Box>
-                  <Box>
-                    <Tv /> <Cloud />
-                  </Box>
-                </TableCell>
-                <TableCell align="center">3</TableCell>
-                <TableCell
-                  
-                  align="center"
-                >
-                  <Box
-                    sx={{
-                      width: "40px",
-                      height: "40px",
-                      borderRadius: "50%",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      "&:hover": {
-                        backgroundColor: "#40BFC1",
-                        color: "#fff",
-                        transition: "0.3s",
-                      },
-                    }}
-                  >
-                    <Visibility />
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                <Visibility />
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
