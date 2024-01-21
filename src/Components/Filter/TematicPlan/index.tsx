3a692760a4e070b5676c80c19c1437b753027ba3
Timur <raxmatovtimurjava@gmail.com>
2024-01-21 17:57:34 +0500

    added subject gets

--- src/Components/Filter/TematicPlan/index.tsx
+++ src/Components/Filter/TematicPlan/index.tsx
@@ -1,21 +1,37 @@
-import {
-  Bookmark,
-  KeyboardArrowDown,
-} from "@mui/icons-material";
+import { Bookmark, KeyboardArrowDown } from "@mui/icons-material";
 import { Box, Button, Menu, MenuItem } from "@mui/material";
 import { Typography } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import ClassFilterMenu from "../../Menu/TematicPlansFilter/FilterClassMenu";
 import QuarterFilterMenu from "../../Menu/TematicPlansFilter/FilterQuarterMenu";
+import { base_api } from "../../../Api/base.api";
+import { useAppDispatch, useAppSelector } from "../../../store";
+import { EditScience } from "../../../store/slices/science";
 
 const TematicPlanFilter = () => {
+  const dispatch = useAppDispatch()
+  const state = useAppSelector((state) => state.science.sciences)  
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl)
-  
+  const open = Boolean(anchorEl);
+
+  const getAllSubjects = async (): Promise<void> => {
+    try {
+      const { data } = await base_api.findAllSciences();
+      if(data.code === 200) {
+        dispatch(EditScience(data.data))
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllSubjects()
+  } , [])
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -28,7 +44,7 @@ const TematicPlanFilter = () => {
         display: "flex",
         justifyContent: "start",
         gap: "1.3rem",
-        flexWrap: "wrap"
+        flexWrap: "wrap",
       }}
     >
       <Box>
@@ -41,7 +57,7 @@ const TematicPlanFilter = () => {
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
-            color: "grey"
+            color: "grey",
           }}
           id="demo_subject_button"
           aria-controls={open ? "demo-subject-menu" : undefined}
@@ -70,11 +86,10 @@ const TematicPlanFilter = () => {
             horizontal: "left",
           }}
         >
-          <MenuItem onClick={handleClose}>Fizika</MenuItem>
-          <MenuItem onClick={handleClose}>Algebra</MenuItem>
-          <MenuItem onClick={handleClose}>Geometriya</MenuItem>
-          <MenuItem onClick={handleClose}>Tarix</MenuItem>
-          <MenuItem onClick={handleClose}>Kimyo</MenuItem>
+          {state.map((item:any) => (
+            <MenuItem key={item.id} onClick={handleClose}>{item.science_name}</MenuItem>
+          ))}
+         
         </Menu>
       </Box>
       <ClassFilterMenu />
